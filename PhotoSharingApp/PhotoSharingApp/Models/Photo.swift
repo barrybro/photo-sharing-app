f229dbdec12cbdd861db24d386f8bfa924337f30
Barry Brown <barryjbro@gmail.com>
2017-04-17 23:19:16 -0500

    add unit tests for photo detail viewModel

--- PhotoSharingApp/PhotoSharingApp/Models/Photo.swift
+++ PhotoSharingApp/PhotoSharingApp/Models/Photo.swift
@@ -10,14 +10,7 @@ import Foundation
 
 struct Photo {
     let photoID: String
-    let secret: String
-    let server: String
-    let farm: Int
     let title: String
-    let isPrimary: String
-    let isPublic: Int
-    let isFriend: Int
-    let isFamily: Int
     let thumbnailURL: String
     let imageURL: String
     let dateTaken: String
@@ -26,14 +19,7 @@ struct Photo {
 
     init?(dictionary: [String: Any]) {
         guard let photoID = dictionary["id"] as? String,
-        let secret = dictionary["secret"] as? String,
-        let server = dictionary["server"] as? String,
-        let farm = dictionary["farm"] as? Int,
         let title = dictionary["title"] as? String,
-        let isPrimary = dictionary["isprimary"] as? String,
-        let isPublic = dictionary["ispublic"] as? Int,
-        let isFriend = dictionary["isfriend"] as? Int,
-        let isFamily = dictionary["isfamily"] as? Int,
         let thumbnailURL = dictionary["url_sq"] as? String,
         let imageURL = dictionary["url_m"] as? String,
         let dateTaken = dictionary["datetaken"] as? String,
@@ -43,14 +29,7 @@ struct Photo {
                 return nil
         }
         self.photoID = photoID
-        self.secret = secret
-        self.server = server
-        self.farm = farm
         self.title = title
-        self.isPrimary = isPrimary
-        self.isPublic = isPublic
-        self.isFriend = isFriend
-        self.isFamily = isFamily
         self.thumbnailURL = thumbnailURL
         self.imageURL = imageURL
         self.dateTaken = dateTaken
---
+++ PhotoSharingApp/PhotoSharingAppTests/PhotoDetailViewModelTests.swift
@@ -0,0 +1,62 @@
+//
+//  PhotoDetailViewModelTests.swift
+//  PhotoSharingApp
+//
+//  Created by Barry Brown on 4/17/17.
+//  Copyright © 2017 barrybrown. All rights reserved.
+//
+
+import XCTest
+@testable import PhotoSharingApp
+
+class PhotoDetailViewModelTests: XCTestCase {
+    func testButtonTitle() {
+        let viewModel = mockViewModel()
+        let expectedTitle = "Share Photo"
+        XCTAssertEqual(expectedTitle, viewModel.shareButtonTitle)
+    }
+
+    func testPhotoTitle() {
+        let viewModel = mockViewModel()
+        let expectedTitle = "title"
+        XCTAssertEqual(expectedTitle, viewModel.photoTitle())
+    }
+
+    func testPhotoURL() {
+        let viewModel = mockViewModel()
+        let expectedValue = "url_m"
+        XCTAssertEqual(expectedValue, viewModel.photoURL())
+    }
+
+    func testPhotoDateTaken() {
+        let viewModel = mockViewModel()
+        let expectedString = "Taken on Mar 4, 2017"
+        XCTAssertEqual(expectedString, viewModel.photoDateTaken())
+    }
+
+    func testPhotoViewCountSingleView() {
+        let viewModel = mockViewModel(viewCount: "1")
+        let expectedString = "1 view"
+        XCTAssertEqual(expectedString, viewModel.photoViewCount())
+    }
+
+    func testPhotoViewCountMultipleViews() {
+        let viewModel = mockViewModel()
+        let expectedString = "4 views"
+        XCTAssertEqual(expectedString, viewModel.photoViewCount())
+    }
+
+    // MARK: - Private
+
+    fileprivate func mockViewModel(viewCount: String = "4") -> PhotoDetailViewModel {
+        let dictionary: [String: Any] = ["id": "123",
+                                         "title": "title",
+                                         "url_sq": "url_sq",
+                                         "url_m": "url_m",
+                                         "datetaken": "2017-03-04 09:59:55",
+                                         "views": viewCount,
+                                         "tags": "tags wedding"]
+        let photo = Photo(dictionary: dictionary)!
+        return PhotoDetailViewModel(photo: photo)
+    }
+}
